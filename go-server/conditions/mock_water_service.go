e43de3358e1fad8c5d9ad431d0244d1b9ba2d301
Verena Lechner <v.m.s.lechner@gmail.com>
2025-04-18 16:56:28 +0200

    fixed integration tests in backend

--- go-server/conditions/mock_water_service.go
+++ go-server/conditions/mock_water_service.go
@@ -1,26 +1,21 @@
 package conditions
 
+import "time"
+
 // MockWaterService is a mock implementation of WaterDataProvider for testing.
 type MockWaterService struct{}
 
 // GetLatestWaterLevelAndFlow implements WaterDataProvider.
 func (m *MockWaterService) GetLatestWaterLevelAndFlow() (*WaterLevelAndFlow, error) {
-	panic("unimplemented")
-}
-
-func (m *MockWaterService) GetCurrentWeather() (*WeatherData, error) {
-	return &WeatherData{
-		Temp:      20.0,
-		Condition: "Clear",
+	return &WaterLevelAndFlow{
+		Level:       143.0,
+		Flow:        9.5,
+		RequestDate: time.Now().Format(time.RFC3339),
 	}, nil
 }
 
 func (m *MockWaterService) GetCachedWaterTemperature() (float64, error) {
 	return 16.5, nil
-}
-
-func (m *MockWaterService) GetCurrentWaterConditions() (float64, float64, error) {
-	return 143.0, 9.5, nil
 }
 
 func (m *MockWaterService) GetLatestWaterTemperature() (float64, error) {
--- go-server/conditions/mock_weather.go
+++ go-server/conditions/mock_weather.go
@@ -1,6 +1,3 @@
-//go:build test
-// +build test
-
 package conditions
 
 func GetCurrentWeather() (*WeatherData, error) {
